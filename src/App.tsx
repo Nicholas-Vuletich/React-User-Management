2cddfeca5f31e55e57e33d1ca925e6b78c85db20
Nikola Vuletić <nikolavuletic412@gmail.com>
2025-03-29 09:35:09 +0100

    Postavlje useState za upravljanje korisnicima s pocetnim podatcima

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import React from "react";
+import { useState } from 'react'
 
+type KorisnikType = {
+    id: number;
+    ime: string;
+    email: string;
+    admin?: boolean;
+};
+
+const pocetniKorisnici: KorisnikType[] = [
+    {id: 1, ime: "Marko", email: "marko@email.com"},
+    {id: 2, ime: "Ana", email: "ana@gmail.com", admin: true},
+];
 
 
 function App() {
-  
+  const [korisnici, setKorisnici] = useState<KorisnikType[]>(pocetniKorisnici);
+  console.log(korisnici)
 }
+
+
 
 export default App
--- src/components/formaZaDodavanje.tsx
+++ src/components/formaZaDodavanje.tsx
@@ -1,13 +1,2 @@
 
 
-type KorisnikType = {
-    id: number;
-    ime: string;
-    email: string;
-    admin?: boolean;
-};
-
-const pocetniKorisnici: KorisnikType[] = [
-    {id: 1, ime: "Marko", email: "marko@email.com"},
-    {id: 2, ime: "Ana", email: "ana@gmail.com", admin: true},
-];
